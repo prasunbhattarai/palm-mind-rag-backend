8afd91575385116500188db143a518efa21b4228
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2026-06-09 23:36:11 +0545

    upadated main.py

--- app/main.py
+++ app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from app.api.booking import router as booking_router
 from app.api.ingest import router as ingest_router
 from app.api.chat import router as chat_router
+from app.db.models import Base
+from app.db.postgres import engine
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+
+app.include_router(chat_router)
+app.include_router(ingest_router)
+app.include_router(booking_router)
 
 
 
