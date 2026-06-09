efaae7b7af2611166ec1019156d18cb198677eba
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2026-06-09 22:43:32 +0545

    addedengine and table schema

---
+++ app/db/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, DateTime, Date, Time
+from sqlalchemy.orm import declarative_base
+from datetime import datetime
+
+
+
+Base = declarative_base()
+
+
+class Document(Base):
+    __tablename__ = "document"
+
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String, nullable=False)
+
+
+
+class Booking(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    interview_date = Column(Date, nullable=False)
+    interview_time = Column(Time, nullable=False)
+    booking_time = Column(DateTime, default=datetime.now)
--- app/db/postgres.py
+++ app/db/postgres.py
@@ -1,8 +1,17 @@
-import psycopg2
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-conn = psycopg2.connect(
-    host = "localhost",
-    port = "5432",
-    database = "db"
-    
+DATABASE_URL = (
+    f"postgresql://{os.getenv('POSTGRES_USER')}:"
+    f"{os.getenv('POSTGRES_PASSWORD')}@postgres:5432/"
+    f"{os.getenv('POSTGRES_DB')}"
+)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
