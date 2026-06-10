70aa52ad7b37faea4a62f4f2cae17b6762c05bb9
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2026-06-10 12:25:21 +0545

    added two chuncking methods

---
+++ app/utils/chunking.py
@@ -0,0 +1,22 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+def fix_chunking(text:str, chunck_size: int = 100, overlap: int = 20):
+    chunks =[]
+    start = 0
+
+    while start < len(text):
+        end = start + chunck_size
+
+        chunks.append(text[start:end])
+
+        start += chunck_size - overlap
+    
+    return chunks
+
+
+def recursive_chunk(text:str, chuck_size: int = 200, overlap: int =40):
+    
+    splitter = RecursiveCharacterTextSplitter(chuck_size=chuck_size, chuck_overlap = overlap)
+
+    return splitter.split_text(text)
